b6429f3dea2512dca2df5e639c3773acad9a8958
demepty <dgonzalezpty@gmail.com>
2018-02-13 21:19:08 -0500

    mejoras

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/style.css"> <!-- Este css debe ir primero por si se necesita perosnalizar alguna clase de bootstraps-->
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <script src="assets/js/jquery-3.3.1.min.js"></script>
     <script src="assets/js/prueba.js"></script>
@@ -13,16 +14,28 @@
     <title>Document</title>
 </head>
 <body>
-<div class="container">
-    <label for="elid">introduce el id</label>
-    <input type="text" id="elid">
-    <button onclick=creo()> <a href="#"> CLICK </a> </button>
-</div>
-
-<div id="dato">
-<input type="text" id="datito">
-
-</div>
-    
+    <header>
+    <div class="cabecera">
+            <div class="container">
+                <h1> Crud con php jax y bootstrap 4</h1>
+            </div>
+    </div>
+    </header>
+    <div class="container">        
+            <div class="row">
+                <div class="col-md-4">
+                    <h3 class="text-center"> Consulta via ajax</h3>                   
+                </div>
+                <div class="col-md-4">
+                    <label for="elid">Id usuario</label>
+                    <input type="text" id="elid">
+                    <button onclick=creo()>  CLICK  </button>                                       
+                </div>
+                <div class="col-md-4">
+                    <label for="result"> Resultado</label>
+                    <input type="text" id="datito">
+                </div>
+            </div>    
+    </div>
 </body>
 </html>
