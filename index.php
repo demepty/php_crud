8992d2c92043410ab7d1398e6a3b397c84fdfc52
demepty <dgonzalezpty@gmail.com>
2018-02-13 20:27:43 -0500

    Añadiendo bootstrap 4

--- index.php
+++ index.php
@@ -5,12 +5,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <script src="assets/js/jquery-3.3.1.min.js"></script>
     <script src="assets/js/prueba.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
     <title>Document</title>
 </head>
 <body>
-<div>
+<div class="container">
     <label for="elid">introduce el id</label>
     <input type="text" id="elid">
     <button onclick=creo()> <a href="#"> CLICK </a> </button>
