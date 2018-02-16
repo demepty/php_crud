392cf67b105b0f150013acfd1f5346bdcdc71735
demepty <dgonzalezpty@gmail.com>
2018-02-15 23:07:48 -0500

    Añadiremos datatable para las tablas

--- conn/conexion.php
+++ conn/conexion.php
@@ -2,7 +2,7 @@
 $dbserver = "localhost";
 $dbuser = "root";
 $password = "";
-$dbname = "crud2";
+$dbname = "producto";
 
 $con = mysqli_connect ($dbserver, $dbuser, $password) or die ("No se ha podido conectar al servidor de Base de datos");
 $db = mysqli_select_db( $con, $dbname) or die ( "Upps! Pues va a ser que no se ha podido conectar a la base de datos" );
